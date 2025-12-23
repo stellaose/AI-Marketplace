8ed0285651a6161a7d74b26e9afb2f2cb3a55183
Stella Oseyomon <admin@SF-MACBOOK-010.local>
2025-12-23 18:17:39 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Footer from './components/layouts/Footer'
+import Header from './components/layouts/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header:React.FC = () => {
+  return (
+    <div>
+      <div>
+        <p>AI-Marketplace</p>
+      </div>
+    </div>
+  )
+}
+
+export default Header
